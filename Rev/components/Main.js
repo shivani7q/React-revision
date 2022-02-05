01c9597aaf1bf69e39b864cc0cf2384d44ed49c3
Shivani Singh <94933127+shivani7q@users.noreply.github.com>
2022-02-05 19:41:24 +0530

    React revision-1

---
+++ Rev/components/Main.js
@@ -0,0 +1,15 @@
+/**
+Challenge: Build the main section!
+
+Skip 2 aspects of the design for now:
+1. The colored bullets in the list
+2. The larger React logo on the side
+
+Those will be separate challenges coming up.
+*/
+
+import React from "react"
+
+export default function Main() {
+    return <h1>Main component</h1>
+}
